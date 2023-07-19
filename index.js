15b6b397b216283961fa2770e2dd66540a3b985f
katherinemoore <katherinemoore789@gmail.com>
2023-07-18 18:48:12 -0700

    Created card decks

--- index.js
+++ index.js
@@ -1,57 +1,45 @@
-let cards = [
-  {
-    id: "star",
-    image: ""
-  },
-  {
-    id: "sun",
-    image: ""
-  },
-  {
-    id: "moon",
-    image: ""
-  },
-  {
-    id: "comet",
-    image: ""
-  }
-];
+import {superMario, avengers} from 'card_decks.js';
+
 
 //Creates a shuffled array containing two of each card
 function shuffleCards(cards) {
-  let numCards = cards.length;
-  let shuffledArray = [];
+    let numCards = cards.length;
+    let shuffledArray = [];
 
-  //Puts one of each card in a random location
-  cards.map((card) => {
-    let index = Math.floor(Math.random() * numCards * 2);
-    if (!shuffledArray[index]) {
-      shuffledArray[index] = card;
-    } else {
-      while (shuffledArray[index]) {
-        index = Math.floor(Math.random() * numCards * 2);
-      } //Ensures no overwrite
-      shuffledArray[index] = card;
-    }
-  });
-
-  //Puts a second of each card in a random location
-  cards.map((card) => {
-    let index = Math.floor(Math.random() * numCards * 2);
-    if (!shuffledArray[index]) {
-      shuffledArray[index] = card;
-    } else {
-      while (shuffledArray[index]) {
-        index = Math.floor(Math.random() * numCards * 2);
-      }
-      shuffledArray[index] = card;
-    }
-  });
-
-  return shuffledArray;
+    //Puts one of each card in a random location
+    cards.map((card) => {
+        let index = Math.floor(Math.random()*numCards*2);
+        if (!shuffledArray[index]) {
+            shuffledArray[index] = card;
+        } else {
+            while (shuffledArray[index]) {
+                index = Math.floor(Math.random()*numCards*2);
+            } //Ensures no overwrite
+            shuffledArray[index] = card;
+        }
+    });
+    
+    //Puts a second of each card in a random location
+    cards.map((card) => {
+        let index = Math.floor(Math.random()*numCards*2);
+        if (!shuffledArray[index]) {
+            shuffledArray[index] = card;
+        } else {
+            while (shuffledArray[index]) {
+                index = Math.floor(Math.random()*numCards*2);
+            }
+            shuffledArray[index] = card;
+        }
+    });
+    
+    return shuffledArray;
 }
-console.log(shuffleCards(cards));
+//console.log(shuffleCards(cards));
 
-function flipCard() {}
+function flipCard() {
+    
+}
 
-function checkMatch() {}
+function checkMatch() {
+
+}
