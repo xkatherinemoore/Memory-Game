c1b6aae21f76d4ab629d50bc4b2ede10f43fe33e
Lehron <lehronrobinson@yahoo.com>
2023-07-19 21:24:32 -0400

    creaetd working card flip and proper id and image assigning

--- card_decks.js
+++ card_decks.js
@@ -1,53 +1,80 @@
+export let defaultDeck = [
+  {
+    id: "smile",
+    img: "imgs/emojis/emoji-smile.svg"
+  },
+  {
+    id: "frown",
+    img: "imgs/emojis/emoji-frown.svg"
+  },
+  {
+    id: "sunglasses",
+    img: "imgs/emojis/emoji-sunglasses.svg"
+  },
+  {
+    id: "dizzy",
+    img: "imgs/emojis/emoji-dizzy.svg"
+  },
+  {
+    id: "angry",
+    img: "imgs/emojis/emoji-angry.svg"
+  },
+  {
+    id: "wink",
+    img: "imgs/emojis/emoji-wink.svg"
+  }
+];
+
 export let superMario = [
-    {
-        id: 'mario',
-        image: 'imgs/super-mario.png'
-    },
-    {
-        id: 'mushroom',
-        image: 'imgs/mushroom.png'
-    },
-    {
-        id: 'coins',
-        image: 'imgs/coins.png'
-    },
-    {
-        id: 'plant',
-        image: 'imgs/carnivorous-plant.png'
-    },
-    {
-        id: 'big M',
-        image: 'imgs/super-mario-letter'
-    },
-    {
-        id: 'mario-logo',
-        image: 'imgs/super-mario-text'
-    },
+  {
+    id: "mario",
+    image: "url(./imgs/super-mario.png)"
+  },
+  {
+    id: "mushroom",
+    image: "url(./imgs/mushroom.png)"
+  },
+  {
+    id: "coins",
+    image: "url(./imgs/coins.png)"
+  },
+  {
+    id: "plant",
+    image: "url(./imgs/carnivorous-plant.png)"
+  },
+  {
+    id: "big M",
+    image: "url(./imgs/super-mario-letter.png)"
+  },
+  {
+    id: "mario-logo",
+    image: "url(./imgs/super-mario-text.png)"
+  }
 ];
 
 export let avengers = [
-    {
-        id: 'black panther',
-        image: 'imgs/black-panther.png'
-    },
-    {
-        id: 'hulk',
-        image: 'imgs/hulk@2x.png'
-    },
-    {
-        id: 'captain america',
-        image: 'imgs/shield.png'
-    },
-    {
-        id: 'spiderman',
-        image: 'imgs/spiderman.png'
-    },
-    {
-        id: 'thanos',
-        image: 'imgs/thanos.png'
-    },
-    {
-        id: 'thor',
-        image: 'imgs/thor-hammer.png'
-    },
-]
+  {
+    id: "black panther",
+    image: "imgs/black-panther.png"
+  },
+  {
+    id: "hulk",
+    image: "imgs/hulk@2x.png"
+  },
+  {
+    id: "captain america",
+    image: "imgs/shield.png"
+  },
+  {
+    id: "spiderman",
+    image: "imgs/spiderman.png"
+  },
+  {
+    id: "thanos",
+    image: "imgs/thanos.png"
+  },
+  {
+    id: "thor",
+    image: "imgs/thor-hammer.png"
+  }
+];
--- index.js
+++ index.js
@@ -1,60 +1,77 @@
-import {superMario, avengers} from 'card_decks.js';
+import { superMario, avengers, defaultDeck } from "./card_decks.js";
 
+let cards = superMario;
+const shuffledArray = shuffleCards(cards);
+console.log(shuffledArray);
 
 const cardList = document.querySelectorAll("section>div");
 const cardListArr = Array.from(cardList);
 
 const selectInput = document.querySelector("select");
 
-selectInput.addEventListener("change", function () {
-  console.log(selectInput.selectedIndex);
-});
+// Change the theme of the card deck
+function changeTheme() {
+  // Mario
+  if (selectInput.selectedIndex === 1) {
+    for (let card of cardListArr) {
+      card.style.backgroundColor = "red";
+    }
+  } else {
+    // Avengers
+    for (let card of cardListArr) {
+      card.style.backgroundColor = "blue";
+    }
+  }
+}
 
-cardListArr.map(function (card) {
-  card.addEventListener("click", function (e) {
-    e.target.style.backgroundColor = "green";
-  });
-});
+function flipCard(e) {
+  console.log(e);
+  e.target.classList.remove();
+  e.target.className = "front";
+
+  let index = e.srcElement.id;
+  let thisCard = shuffledArray[index];
+
+  e.target.style.backgroundImage = thisCard.image;
+}
 
 //Creates a shuffled array containing two of each card
 function shuffleCards(cards) {
-    let numCards = cards.length;
-    let shuffledArray = [];
+  let numCards = cards.length;
+  let shuffledArray = [];
 
-    //Puts one of each card in a random location
-    cards.map((card) => {
-        let index = Math.floor(Math.random()*numCards*2);
-        if (!shuffledArray[index]) {
-            shuffledArray[index] = card;
-        } else {
-            while (shuffledArray[index]) {
-                index = Math.floor(Math.random()*numCards*2);
-            } //Ensures no overwrite
-            shuffledArray[index] = card;
-        }
-    });
-    
-    //Puts a second of each card in a random location
-    cards.map((card) => {
-        let index = Math.floor(Math.random()*numCards*2);
-        if (!shuffledArray[index]) {
-            shuffledArray[index] = card;
-        } else {
-            while (shuffledArray[index]) {
-                index = Math.floor(Math.random()*numCards*2);
-            }
-            shuffledArray[index] = card;
-        }
-    });
-    
-    return shuffledArray;
+  //Puts one of each card in a random location
+  cards.map((card) => {
+    let index = Math.floor(Math.random() * numCards * 2);
+    if (!shuffledArray[index]) {
+      shuffledArray[index] = card;
+    } else {
+      while (shuffledArray[index]) {
+        index = Math.floor(Math.random() * numCards * 2);
+      } //Ensures no overwrite
+      shuffledArray[index] = card;
+    }
+  });
+
+  //Puts a second of each card in a random location
+  cards.map((card) => {
+    let index = Math.floor(Math.random() * numCards * 2);
+    if (!shuffledArray[index]) {
+      shuffledArray[index] = card;
+    } else {
+      while (shuffledArray[index]) {
+        index = Math.floor(Math.random() * numCards * 2);
+      }
+      shuffledArray[index] = card;
+    }
+  });
+
+  return shuffledArray;
 }
+
 //console.log(shuffleCards(cards));
+selectInput.addEventListener("change", changeTheme);
 
-function flipCard() {
-    
-}
-
-function checkMatch() {
-
-}
+cardListArr.forEach((card) => {
+  card.addEventListener("click", flipCard);
+});
