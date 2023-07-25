df1f519daf83363b1e94d4f97103c5c3babbeb79
Lehron <lehronrobinson@yahoo.com>
2023-07-24 22:07:08 -0400

    Completed group assignment

---
+++ gameData.js
@@ -0,0 +1,26 @@
+export let defaultDeck = [
+  "imgs/emojis/happy-emoji.png",
+  "imgs/emojis/frown-emoji.png",
+  "imgs/emojis/sunglasses-emoji.png",
+  "imgs/emojis/dizzy-emoji.png",
+  "imgs/emojis/angry-emoji.png",
+  "imgs/emojis/wink-emoji.png"
+];
+
+export let superMario = [
+  "./imgs/mario/super-mario.png",
+  "./imgs/mario/mushroom.png",
+  "./imgs/mario/coins.png",
+  "./imgs/mario/carnivorous-plant.png",
+  "./imgs/mario/super-mario-letter.png",
+  "./imgs/mario/super-mario-text.png"
+];
+
+export let avengers = [
+  "imgs/avengers/black-panther.png",
+  "imgs/avengers/hulk@2x.png",
+  "imgs/avengers/shield.png",
+  "imgs/avengers/spiderman.png",
+  "imgs/avengers/thanos.png",
+  "imgs/avengers/thor-hammer.png"
+];
--- index.js
+++ index.js
@@ -1,32 +1,51 @@
-import { superMario, avengers, defaultDeck } from "./card_decks.js";
+import { superMario, avengers, defaultDeck } from "./gameData.js";
+// Object used for selecting different themes
+let cardTheme = {
+  basic: defaultDeck,
+  mario: superMario,
+  avengers: avengers
+};
+// Global variables
+let currentTheme = cardTheme.basic;
+let moveCount = 0;
+let setImgsArr = [];
+// HTML elements used for data manipulation
+const cardDeck = Array.from(document.querySelectorAll("#cardDeck > div"));
+const moveCounter = document.getElementById("moveCount");
+const newGame = document.getElementById("newGame");
+const theme = document.querySelector("select");
 
-let cards = superMario;
-const shuffledArray = shuffleCards(cards);
-const cardList = document.querySelectorAll("section>div");
-const cardListArr = Array.from(cardList);
-const selectInput = document.querySelector("select");
-
-let turnCounter = 0;
-// Change the theme of the card deck
-function changeTheme() {
-  // Mario
-  if (selectInput.selectedIndex === 1) {
-    for (let card of cardListArr) {
-      card.style.backgroundColor = "red";
+// Start new game
+function startNewGame() {
+  // Reshuffle deck once a new game is started
+  let shuffledDeck = shuffleImgs(currentTheme);
+  // Capture images for new shuffled deck
+  let imgArray = Array.from(document.querySelectorAll("div>img"));
+  // Reset move counter
+  moveCount = 0;
+  moveCounter.textContent = moveCount;
+  // Reset classes on card elements
+  cardDeck.forEach((card) => {
+    card.classList.remove("front", "match");
+    // Reset cards to initial state with back class
+    if (!card.classList.contains("back")) {
+      card.classList.add("back");
     }
-  } else {
-    // Avengers
-    for (let card of cardListArr) {
-      card.style.backgroundColor = "blue";
+    // If new game started after initial load, remove images from card elements
+    if (card.children.length > 0) {
+      card.removeChild(document.querySelector("div > img"));
     }
-  }
+  });
+  setImgsToCards(cardDeck);
 }
-function shuffleCards(cards) {
-  let numCards = cards.length;
+
+// Shuffle card images
+function shuffleImgs(currentTheme) {
+  let numCards = currentTheme.length;
   let shuffledArr = [];
 
   //Puts one of each card in a random location
-  cards.map((card) => {
+  currentTheme.map((card) => {
     let index = Math.floor(Math.random() * numCards * 2);
     if (!shuffledArr[index]) {
       shuffledArr[index] = card;
@@ -39,7 +58,7 @@ function shuffleCards(cards) {
   });
 
   //Puts a second of each card in a random location
-  cards.map((card) => {
+  currentTheme.map((card) => {
     let index = Math.floor(Math.random() * numCards * 2);
     if (!shuffledArr[index]) {
       shuffledArr[index] = card;
@@ -53,121 +72,124 @@ function shuffleCards(cards) {
 
   return shuffledArr;
 }
-console.log(shuffledArray);
+// Randomly set current game cards images
+function setImgsToCards(cardDeck) {
+  let currentImgs = shuffleImgs(currentTheme);
+  let index = 0;
 
-//Checks two flipped cards to see if they're a match
-// function checkMatch(e) {
-//   let firstCardIndex = cardListArr.findIndex((card) => {
-//     return card.classList.contains("front");
-//   });
-//   let firstCard = shuffledArray[firstCardIndex];
+  console.log(currentImgs);
 
-//   let secondCardIndex =
-//     11 -
-//     cardListArr.reverse().findIndex((card) => {
-//       return card.classList.contains("front");
-//     });
-
-//   let secondCard = shuffledArray[secondCardIndex];
-//   console.log(`first card: ${firstCard.id}`);
-//   console.log(`second card: ${secondCard.id}`);
-//   console.log(firstCardIndex);
-//   console.log(secondCardIndex);
-
-//   //If matched, keep face up. Otherwise, flip back facedown
-//   if (firstCard.id === secondCard.id) {
-//     cardListArr.forEach((card) => {
-//       if (card.classList.contains("front")) {
-//         card.classList.remove();
-//         card.classList.add("match");
-//         console.log("match");
-//       }
-//     });
-//   } else {
-//     cardListArr.forEach((card) => {
-//       if (card.classList.contains("front")) {
-//         card.classList.remove();
-//         card.classList.add("back");
-//         card.removeAttribute("style");
-//         console.log("not a match");
-//       }
-//     });
-//   }
-
-//   turnCount++;
-// }
-
-function checkMatch() {
-  let firstCardIndex = cardListArr.findIndex((card) => {
-    return card.className === "front";
+  cardDeck.forEach((card) => {
+    // Create and append card img element to current game deck
+    const newImg = document.createElement("img");
+    newImg.setAttribute("src", currentImgs[index]);
+    newImg.classList.add("hide");
+    card.appendChild(newImg);
+    // Used to capture current images being used
+    setImgsArr.push(newImg);
+    index++;
   });
-  let firstCard = shuffledArray[firstCardIndex];
-
-  let reverseArr = cardListArr.reverse();
-  let secondCardIndex =
-    11 -
-    reverseArr.findIndex((card) => {
-      console.log(card.className);
-      return card.className === "front";
-    });
-  let secondCard = shuffledArray[secondCardIndex];
-
-  console.log(secondCardIndex);
-  console.log(secondCard);
-
-  if (firstCard.id === secondCard.id) {
-    console.log("MATCH");
-    cardListArr.forEach((card) => {
-      if (card.classList.contains("front")) {
-        card.classList.remove("front");
-        card.classList.add("match");
-      }
-    });
-  } else {
-    console.log("NOPE");
-    setTimeout(() => {
-      cardListArr.forEach((card) => {
-        if (card.classList.contains("front")) {
-          card.classList.remove("front");
-          card.classList.add("back");
-          card.removeAttribute("style");
-        }
-      });
-    }, 5000);
-  }
-
-  turnCounter++;
 }
-
-function compareCardsInitMatch() {
-  let cardsFlipped = cardListArr.filter((card) => {
-    return card.className === "front";
-  });
-
-  if (cardsFlipped.length === 2) {
-    checkMatch();
-  }
-}
-// Flip cards and add the id and source image to the html element and background
+// Flip card when clicked
 function flipCard(e) {
-  console.log(e);
-  e.target.classList.remove();
-  e.target.className = "front";
+  // Change class on card img to make visible
+  e.target.classList.remove("back");
+  e.target.children[0].classList.remove("hide");
+  e.target.classList.add("front");
+  e.target.children[0].classList.add("show");
 
-  let index = e.srcElement.id;
-  let thisCard = shuffledArray[index];
+  // Check if 2 cards are currently flipped
+  checkFlippedCards();
+}
+// Compare 2 flipped cards to check if they're a match
+function compareCards(flippedCards) {
+  // Capture flipped cards for comparison in variables
+  let firstCard = flippedCards[0];
+  let secondCard = flippedCards[1];
+  // Add a move to the move counter every time 2 cards are compared
 
-  e.target.style.backgroundImage = thisCard.image;
-  console.log(e);
+  // Check if the 2 flipped cards are a match and what to do based on the result
+  firstCard.children[0].getAttribute("src") ===
+  secondCard.children[0].getAttribute("src")
+    ? (firstCard.classList.add("match"),
+      secondCard.classList.add("match"),
+      firstCard.classList.remove("front"),
+      secondCard.classList.remove("front"),
+      console.log(firstCard, secondCard))
+    : setTimeout(() => {
+        firstCard.classList.remove("front");
+        secondCard.classList.remove("front");
+        firstCard.children[0].classList.remove("show");
+        secondCard.children[0].classList.remove("show");
+        firstCard.classList.add("back");
+        secondCard.classList.add("back");
+        firstCard.children[0].classList.add("hide");
+        secondCard.children[0].classList.add("hide");
 
-  compareCardsInitMatch();
+        console.log(firstCard, secondCard);
+      }, 500);
+
+  setTimeout(() => {
+    hasPlayerWon();
+  }, 500);
+  // Update move count
+  moveCount++;
+  moveCounter.textContent = moveCount;
+}
+// Filter through card deck array to check the number of flipped cards
+function checkFlippedCards() {
+  let flippedCards = cardDeck.filter((card) => {
+    return card.classList.contains("front");
+  });
+  // Check number of flip cards
+  flippedCards.length === 2
+    ? compareCards(flippedCards)
+    : console.log("cards flipped: " + flippedCards.length);
+}
+// Check if player has won
+function hasPlayerWon() {
+  let matchesMade = cardDeck.filter((card) => card.classList.contains("match"));
+
+  if (matchesMade.length === cardDeck.length) {
+    alert("YOU WIN!!! Play again?");
+  }
 }
 
-//Creates a shuffled array containing two of each card
-
-//console.log(shuffleCards(cards));
-selectInput.addEventListener("change", changeTheme);
-
-cardListArr.forEach((card) => {
+function changeTheme() {
+  let chosenTheme = theme.options[theme.selectedIndex].text;
+  console.log(chosenTheme);
+  // Used to handle theme changes
+  switch (chosenTheme) {
+    case "Super Mario":
+      currentTheme = cardTheme.mario;
+      cardDeck.forEach((card) => {
+        card.classList.remove("avengers", "basic");
+        card.classList.add("mario");
+      });
+      break;
+    case "Avengers":
+      currentTheme = cardTheme.avengers;
+      cardDeck.forEach((card) => {
+        card.classList.remove("mario", "basic");
+        card.classList.add("avengers");
+      });
+      break;
+    case "Basic":
+      currentTheme = cardTheme.basic;
+      cardDeck.forEach((card) => {
+        card.classList.remove("avengers", "mario");
+        card.classList.add("basic");
+      });
+      break;
+  }
+  startNewGame();
+}
+// Event Listeners
+cardDeck.map((card) => {
   card.addEventListener("click", flipCard);
+  newGame.addEventListener("click", startNewGame);
 });
+
+theme.addEventListener("change", changeTheme);
+
+startNewGame();
